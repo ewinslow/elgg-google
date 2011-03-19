925445c64d6b03b05598ccf3c1971a22cc72cc45
Evan Winslow <evan@elgg.org>
2011-03-18 19:27:44 -0700

    Updated for 1.8
    Updated to asynchronous tracking code
    Removed Google Wave support

--- start.php
+++ start.php
@@ -1,9 +1,10 @@
 <?php
 
-function google_init()
-{
-	elgg_extend_view('footer/analytics', 'scripts/google/analytics');
-	elgg_extend_view('metatags', 'google/siteverification');
+function google_init() {
+	elgg_extend_view('page/elements/head', 'google/siteverification');
+	elgg_extend_view('page/elements/head', 'scripts/google/analytics', 1000);
+
+	elgg_register_js('google.jsapi', '//www.google.com/jsapi', 'footer');
 }
 
-register_elgg_event_handler('init', 'system', 'google_init');
+elgg_register_event_handler('init', 'system', 'google_init');
--- views/default/plugins/google/settings.php
+++ views/default/plugins/google/settings.php
@@ -5,10 +5,10 @@ $google = $vars['entity'];
 ?>
 <div>
 	<label>Google Analytics tracking code</label>
-	<?php 
+	<?php
 		echo elgg_view('input/text', array(
 			'value' => $google->tracking_code,
-			'internalname' => 'params[tracking_code]',
+			'name' => 'params[tracking_code]',
 			'placeholder' => 'UA-XXXXXX-XX',
 		));
 	?>
@@ -16,10 +16,10 @@ $google = $vars['entity'];
 
 <div>
 	<label>Google Webmaster Tools site verification code</label>
-	<?php 
+	<?php
 		echo elgg_view('input/text', array(
 			'value' => $google->site_verification_code,
-			'internalname' => 'params[site_verification_code]',
+			'name' => 'params[site_verification_code]',
 			'placeholder' => 'Site verification code',
 		));
 	?>
