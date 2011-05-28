9b63c329c8d71e7de32378e44328cc54476abfc0
Evan Winslow <evan@elgg.org>
2011-05-27 17:57:39 -0700

    Relocated google analytics script to simplify directory structure of plugin

--- start.php
+++ start.php
@@ -2,7 +2,7 @@
 
 function google_init() {
 	elgg_extend_view('page/elements/head', 'google/siteverification');
-	elgg_extend_view('page/elements/head', 'scripts/google/analytics', 1000);
+	elgg_extend_view('page/elements/head', 'google/analytics', 1000);
 
 	elgg_register_js('google.jsapi', '//www.google.com/jsapi', 'footer');
 	
